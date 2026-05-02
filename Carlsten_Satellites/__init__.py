558d785e97c51ca545a395ec50592af6b6be142b
JohnBrown97 <brownjonathon97@gmail.com>
2026-05-02 15:47:40 -0400

    Made some adjustment to using the dwarfs object. Now I can utilize the masked version fo the files from Ben's code, as well as apply the extinction correct + photometric culls within the package.

--- Carlsten_Satellites/__init__.py
+++ Carlsten_Satellites/__init__.py
@@ -3,8 +3,12 @@ from .dwarfs import Dwarf
 #expose modules
 from . import Plotting
 from . import Analysis
+from . import Half_Light_Radius
+from . import colors
 
 __all__ = ["Dwarf",
 "Plotting", 
-"Analysis"
+"Analysis", 
+"Half_Light_Radius", 
+"colors"
 ]
